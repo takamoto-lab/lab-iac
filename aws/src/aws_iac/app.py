b8b1ff1c26c291b886f90f9f0d4cec4bb9c371ee
takamoto <tkfmnkn@gmail.com>
2022-08-14 15:08:25 +0900

    AccountBudget クラスを追加

--- aws/src/aws_iac/app.py
+++ aws/src/aws_iac/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 
+from aws_iac.stacks.account_budget import AccountBudget
+
 
 if __name__ == "__main__":
     app = cdk.App()
     env = cdk.Environment(account="715861157510", region="ap-northeast-1")
+    AccountBudget(app, AccountBudget.__name__, env=env)
     app.synth()
