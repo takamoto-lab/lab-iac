2d93d80dd486a4a36d191608f995e11a4ec4012a
takamoto <tkfmnkn@gmail.com>
2023-01-08 04:13:54 +0900

    具体的な設定値を反映

--- terraform/services/auth0/modules/auth0_logging/main.tf
+++ terraform/services/auth0/modules/auth0_logging/main.tf
@@ -4,5 +4,6 @@ resource "aws_iam_user" "iam-user-auth0-logging" {
 }
 
 resource "aws_iam_user_policy_attachment" "iam-policy-attachment-user-auth0-logging" {
-
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+  user = "auth0-logging"
 }
