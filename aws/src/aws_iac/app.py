680d5cb4d810f67180741d45904169aa740f6cbc
takamoto <tkfmnkn@gmail.com>
2023-02-05 05:37:02 +0900

    Delete github_actions_oidc stack

--- aws/src/aws_iac/app.py
+++ aws/src/aws_iac/app.py
@@ -2,12 +2,10 @@
 import aws_cdk as cdk
 
 from aws_iac.stacks.minecraft_server import MinecraftServer
-from aws_iac.stacks.github_actions_oidc import GitHubActionsOIDC
 
 
 if __name__ == "__main__":
     app = cdk.App()
     env = cdk.Environment(account="715861157510", region="ap-northeast-1")
     MinecraftServer(app, MinecraftServer.__name__, env=env)
-    GitHubActionsOIDC(app, GitHubActionsOIDC.__name__, env=env)
     app.synth()
