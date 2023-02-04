c17af33c5b7b0375815ce7efaa4dc2b50798b703
takamoto <tkfmnkn@gmail.com>
2023-02-05 04:10:59 +0900

    フォーマッタを適用

--- terraform/services/github-actions-oidc/modules/github_actions_oidc/main.tf
+++ terraform/services/github-actions-oidc/modules/github_actions_oidc/main.tf
@@ -1,8 +1,8 @@
 
 # 参考: https://zenn.dev/miyajan/articles/github-actions-support-openid-connect
 resource "aws_iam_openid_connect_provider" "github_actions_oidc_provider" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
   # thumbprintについて: https://qiita.com/minamijoyo/items/eac99e4b1ca0926c4310
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
