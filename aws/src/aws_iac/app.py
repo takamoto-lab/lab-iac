08e9ed6560c2bb8dd448924b769ada90d90c2235
takamoto <tkfmnkn@gmail.com>
2023-02-05 02:17:11 +0900

    Delete account_budget stack (#13)

--- aws/src/aws_iac/app.py
+++ aws/src/aws_iac/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 
-from aws_iac.stacks.account_budget import AccountBudget
 from aws_iac.stacks.minecraft_server import MinecraftServer
 from aws_iac.stacks.github_actions_oidc import GitHubActionsOIDC
 
@@ -9,7 +8,6 @@ from aws_iac.stacks.github_actions_oidc import GitHubActionsOIDC
 if __name__ == "__main__":
     app = cdk.App()
     env = cdk.Environment(account="715861157510", region="ap-northeast-1")
-    AccountBudget(app, AccountBudget.__name__, env=env)
     MinecraftServer(app, MinecraftServer.__name__, env=env)
     GitHubActionsOIDC(app, GitHubActionsOIDC.__name__, env=env)
     app.synth()
