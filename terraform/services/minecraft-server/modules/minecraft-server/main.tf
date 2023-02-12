9b13fb5711896e58ea988624cae909c0ed3f146b
takamoto <tkfmnkn@gmail.com>
2023-02-12 22:20:35 +0900

    AZ を増やす (#26)
    * public subnet を削除
    
    * az を増やす

--- terraform/services/minecraft-server/modules/minecraft-server/main.tf
+++ terraform/services/minecraft-server/modules/minecraft-server/main.tf
@@ -1,6 +1,6 @@
 locals {
   vpc_cidr           = "10.0.0.0/16"
-  availability_zones = ["ap-northeast-1a", "ap-northeast-1c"]
+  availability_zones = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
   cidr_blocks        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
