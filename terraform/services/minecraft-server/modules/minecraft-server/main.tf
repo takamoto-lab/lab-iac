c9fd3bc151bcbe0b076edcbf8b445e7c6160893b
takamoto <tkfmnkn@gmail.com>
2023-02-12 21:59:09 +0900

    public subnet をもとに戻す

--- terraform/services/minecraft-server/modules/minecraft-server/main.tf
+++ terraform/services/minecraft-server/modules/minecraft-server/main.tf
@@ -1,7 +1,7 @@
 locals {
   vpc_cidr           = "10.0.0.0/16"
   availability_zones = ["ap-northeast-1a", "ap-northeast-1c"]
-  cidr_blocks        = ["10.0.1.0/24", "10.0.2.0/24"]
+  cidr_blocks        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 module "vpc" {
