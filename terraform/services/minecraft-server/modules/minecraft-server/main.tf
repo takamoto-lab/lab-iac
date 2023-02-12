4cfdb3cc2a74484cd3035416b39e43e28d2e094d
takamoto <tkfmnkn@gmail.com>
2023-02-12 22:29:30 +0900

    EFS のマウント設定を追加 (#27)

--- terraform/services/minecraft-server/modules/minecraft-server/main.tf
+++ terraform/services/minecraft-server/modules/minecraft-server/main.tf
@@ -14,10 +14,26 @@ module "vpc" {
   public_subnets = local.cidr_blocks
 }
 
+data "aws_subnet" "subnets" {
+  for_each          = toset(local.availability_zones)
+  vpc_id            = module.vpc.vpc_id
+  availability_zone = each.key
+}
+
 module "efs" {
   source = "terraform-aws-modules/efs/aws"
   version = "1.1.1"
 
   name   = "minecraft-server"
+  mount_targets = {
+    for az, subnet in data.aws_subnet.subnets : az => { subnet_id = "${subnet.id}" }
+  }
+
+  security_group_name   = "minecraft-server_efs"
+  security_group_vpc_id = module.vpc.vpc_id
+  security_group_rules = {
+    vpc = { cidr_blocks = local.cidr_blocks }
+  }
+
   enable_backup_policy = true
 }
